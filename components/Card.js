0bbfe249721ec6dfef6902b73fbc10f8533a4677
xeusteerapat <xeus085@gmail.com>
2020-11-21 11:31:43 +0700

    [update] Card styles

--- components/Card.js
+++ components/Card.js
@@ -1,85 +1,32 @@
 import React from 'react';
 
-const Card = () => {
+const Card = ({ beer }) => {
   return (
-    <section className='our-blog'>
-      <div className='container'>
-        <div className='text-center'>
-          <h2>
-            Our <span>Blog</span>
+    <div className='md:flex shadow-lg  mx-6 md:mx-auto my-40 max-w-lg md:max-w-2xl h-64'>
+      <img
+        className='h-full w-full md:w-1/3  object-contain rounded-lg rounded-r-none pb-5/6 py-4'
+        src={beer.image_url}
+        alt={beer.name}
+      />
+      <div className='w-full md:w-2/3 px-4 py-4 bg-white rounded-lg'>
+        <div className='flex items-center'>
+          <h2 className='text-xl text-gray-800 font-medium mr-auto'>
+            {beer.name}
           </h2>
-          <p>
-            It is a long established fact that a reader will be of a page when
-            established fact looking at its layout.
+          <p className='text-gray-800 font-semibold tracking-tighter'>
+            ABV
+            <i className='text-gray-600'> % </i>
+            {beer.abv}
           </p>
         </div>
-        <div className='row mt-5'>
-          <div className='col'>
-            <div className='card'>
-              <img
-                src='https://images.unsplash.com/photo-1601971935068-fb9281b6deec?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80'
-                alt
-              />
-              <div className='card-body'>
-                <h5 className='card-title'>
-                  <a href='#'>Card Title</a>
-                </h5>
-                <p className='date-author'>
-                  11 March 2020 <span className='author'>By Abbos</span>
-                </p>
-                <p className='card-text'>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Numquam vero eaque animi pariatur fugit excepturi, fuga
-                  delectus facere provident beatae.
-                </p>
-              </div>
-            </div>
-          </div>
-          <div className='col'>
-            <div className='card'>
-              <img
-                src='https://images.unsplash.com/photo-1601971935068-fb9281b6deec?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80'
-                alt
-              />
-              <div className='card-body'>
-                <h5 className='card-title'>
-                  <a href='#'>Card Title</a>
-                </h5>
-                <p className='date-author'>
-                  11 March 2020 <span className='author'>By Abbos</span>
-                </p>
-                <p className='card-text'>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Numquam vero eaque animi pariatur fugit excepturi, fuga
-                  delectus facere provident beatae.
-                </p>
-              </div>
-            </div>
-          </div>
-          <div className='col'>
-            <div className='card'>
-              <img
-                src='https://images.unsplash.com/photo-1601971935068-fb9281b6deec?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80'
-                alt
-              />
-              <div className='card-body'>
-                <h5 className='card-title'>
-                  <a href='#'>Card Title</a>
-                </h5>
-                <p className='date-author'>
-                  11 March 2020 <span className='author'>By Abbos</span>
-                </p>
-                <p className='card-text'>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Numquam vero eaque animi pariatur fugit excepturi, fuga
-                  delectus facere provident beatae.
-                </p>
-              </div>
-            </div>
-          </div>
+        <p className='text-sm text-gray-700 mt-4 mb-2'>{beer.description}</p>
+        <div className='flex items-center justify-end top-auto'>
+          <button className='bg-blue-600 text-gray-200 px-4 py-2 rounded mr-auto hover:underline'>
+            Select
+          </button>
         </div>
       </div>
-    </section>
+    </div>
   );
 };
 
